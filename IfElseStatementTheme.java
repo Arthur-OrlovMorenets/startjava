23905f273ec5b7f4ad85b8e27f337c3ac43811f1
Artur-OrlovMorenets <orloffartur@gmail.com>
2022-06-16 20:13:17 +0300

    добавил решени для второй задачи

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -1,6 +1,6 @@
 public class IfElseStatementTheme {
     public static void main(String[] args) {
-        System.out.println("1. Перевод псевдокода на язык Java");
+        System.out.println("1. Перевод псевдокода на язык Java\n");
         
         int age = 19;
         double height = 1.90;
@@ -14,23 +14,32 @@ public class IfElseStatementTheme {
             System.out.println("Младше 20 лет");
         }
 
-        if(!male) {
+        if (!male) {
             System.out.println("Не мужик!");
         }
 
-        if(height < 1.80) {
+        if (height < 1.80) {
             System.out.println("Меньше 180");
         } else {
         System.out.println("Больше 180!");
         }
 
         firstLetterOfName = name.charAt(0);
-        if(firstLetterOfName == 'M') {
+        if (firstLetterOfName == 'M') {
             System.out.println("Первая буква имени М");
         } else if (firstLetterOfName == 'I') {
             System.out.println("Первая буква имени I");
         } else {
             System.out.println("Первая буква не M и не I");
+        }
+        System.out.println("\n2.Поиск max и min числа\n");
+        int firstVal = 5;
+        int secondVal = 7;
+
+        if(firstVal > secondVal) {
+            System.out.println("Максимальное число: " + firstVal + "\nМинимальное число: " + secondVal);
+        } else {
+            System.out.println("Максимальное число: " + secondVal + "\nМинимальное число: " + firstVal);
         }
     }
 }
