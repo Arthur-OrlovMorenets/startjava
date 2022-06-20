68c210bb53237c5bf79f73cfbce9ee59370cdd7d
Artur-OrlovMorenets <orloffartur@gmail.com>
2022-06-20 18:06:51 +0300

    Мелкие правки формата в ДЗ на тему ветвления

--- IfElseStatementTheme.java
+++ IfElseStatementTheme.java
@@ -83,16 +83,16 @@ public class IfElseStatementTheme {
         System.out.print("В числах " + firstNumber + " и " + secondNumber + " одинаковые цифры");
         if (firstNum1 == firstNum2) {
             System.out.print(" " + firstNum1);
-                if (secondNum1 == secondNum2) {
-                    System.out.print(", " + secondNum1);
-                if (thirdNum1 == thirdNum2) {
+            if (secondNum1 == secondNum2) {
+                System.out.print(", " + secondNum1);
+                    if (thirdNum1 == thirdNum2) {
                     System.out.print(", " + thirdNum1);
                 }
             }
         } else if (secondNum1 == secondNum2) {
             System.out.print(" " + secondNum1);
             if (thirdNum1 == thirdNum2) {
-            System.out.print(" и " + thirdNum1);
+                System.out.print(" и " + thirdNum1);
             }
         } else if (thirdNum1 == thirdNum2) {
             System.out.print(" " + thirdNum1);
@@ -113,7 +113,7 @@ public class IfElseStatementTheme {
         if (srcChar >= 'A' && srcChar <= 'Z') {
             System.out.print(" это большая буква");
         } else if (srcChar >= 'a' && srcChar <= 'z') {
-                System.out.print(" это маленькая буква");
+            System.out.print(" это маленькая буква");
         } else if (srcChar >= 0 && srcChar <= 9) {
             System.out.print(" это число");
         } else {
@@ -130,19 +130,19 @@ public class IfElseStatementTheme {
 
         System.out.println("Сумма вклада: " + depositSum);
         if ( depositSum < 100_000) {
-            System.out.print("Начисленный процент: " + depositRateLess100k+ "% годовых");
+            System.out.print("Начисленный процент: " + depositRateLess100k + "% годовых");
             percentage = depositSum * depositRateLess100k / 100;
-            System.out.println(" (" + percentage +" руб.)");
+            System.out.println(" (" + percentage + " руб.)");
             System.out.println("Итоговая сумма с процентами составляет: " + (depositSum + percentage) + "руб.");
         } else if ( depositSum >= 100_000 && depositSum < 300_000) {
-            System.out.print("Начисленный процент: " + depositRateLess300k+ "% годовых");
+            System.out.print("Начисленный процент: " + depositRateLess300k + "% годовых");
             percentage = depositSum * depositRateLess300k / 100;
             System.out.println(" (" + percentage +" руб.)");
             System.out.println("Итоговая сумма с процентами составляет: " + (depositSum + percentage) + "руб.");
         } else if ( depositSum >= 300_000) {
-            System.out.print("Начисленный процент: " + depositRateMore300k+ "% годовых");
+            System.out.print("Начисленный процент: " + depositRateMore300k + "% годовых");
             percentage = depositSum * depositRateMore300k / 100;
-            System.out.println(" (" + percentage +" руб.)");
+            System.out.println(" (" + percentage + " руб.)");
             System.out.println("Итоговая сумма с процентами составляет: " + (depositSum + percentage) + "руб.");
         }
 
