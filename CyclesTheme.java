2c13e5b8331dd7782a485e28d3a2516eb894ea63
Artur-OrlovMorenets <107355010+Arthur-OrlovMorenets@users.noreply.github.com>
2022-07-20 19:03:52 +0300

    Неготовые решения ДЗ по циклам

---
+++ CyclesTheme.java
@@ -0,0 +1,91 @@
+public class CyclesTheme {
+    public static void main(String[] args) {
+    System.out.println("1. Подсчет суммы четных и нечетных чисел");
+    int even = 0;
+    int uneven = 0;
+    int i = -10;
+    do { 
+        if (i % 2 == 0) {
+            even += i;
+        } else {
+            uneven += i;
+        }
+    i++;
+    } while (i <= 21);
+    System.out.println("в промежутке [-10, 21] сумма четных чисел = " + even + ", а нечетных = " + uneven);
+    
+    System.out.println("\n2. Вывод чисел в интервале (min и max)");
+    int firstValue = 10;
+    int secondValue = 5;
+    int thirdValue = -1;
+    int max;
+    if (firstValue > secondValue && firstValue > thirdValue) {
+        max = firstValue;
+    } else if (secondValue > firstValue && secondValue > thirdValue) {
+        max = secondValue;
+    } else {
+        max = thirdValue;
+    }
+    int min;
+        if (firstValue < secondValue && firstValue < thirdValue) {
+        min = firstValue;
+    } else if (secondValue < firstValue && secondValue < thirdValue) {
+        min = secondValue;
+    } else {
+        min = thirdValue;
+    }
+    System.out.println("Наименьшее число: " + min + ", наибольшее число: " + max);
+    for (int j = max; j >= min; j--) {
+        System.out.print(j + " ");
+    }
+
+    System.out.println("\n\n3. Вывод реверсивного числа и суммы его цифр");
+    int number = 1234;
+    int lastNumber;
+    int sum = 0;
+    while (number > 0) {
+        lastNumber = number % 10;
+        System.out.print(lastNumber);
+        sum += lastNumber;
+        number = number / 10;
+    }
+    System.out.println("\n" + sum);
+
+    System.out.println("\n4. Вывод чисел на консоль в несколько строк");
+    for (int k = 1; k < 24; k += 2) {
+        System.out.printf("%5d", k);
+        if (k >= 9 && k < 11) {
+            System.out.println("\n");
+        }
+        if (k >= 19 && k < 21) {
+            System.out.println("\n");
+        }
+        if (k >=23) {
+            System.out.printf("%5d", 0);
+            System.out.printf("%5d", 0);
+            System.out.printf("%5d", 0);
+        }
+    }
+    
+    System.out.println("\n5. Проверка количества единиц на четность");
+    number = 3141591;
+    int sumOfNumbers = 0;
+    
+    System.out.print("число " + number+ " содержит ");
+    while (number > 0) {
+        lastNumber = number % 10;
+        if (lastNumber == 1) {
+        sumOfNumbers += 1;
+        }
+        number = number / 10;
+    }
+    if (sumOfNumbers % 2 == 0) {
+        System.out.print("четное количество единиц");
+    } else if (sumOfNumbers % 2 != 0) {
+        System.out.print("нечетное количество единиц");
+    }
+    
+    System.out.println("\n6. Отображение фигур в консоли");
+     
+    }
+}
